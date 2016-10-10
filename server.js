fd6589188f31507421630eec19711c1835ab72bd
Dee Alnz <davidalnz@gmail.com>
2016-10-10 13:03:03 +0800

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+var _ = require("underscore");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,15 +21,8 @@ app.get("/todos", function(req, res){
 //GET /todos/:id
 app.get("/todos/:id", function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	//Iterate of todos array. Find the match
-	todos.forEach(function(todo){
-		if(todoId === todo.id){
-			matchedTodo = todo;
-		}
-
-	});
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -40,12 +34,20 @@ app.get("/todos/:id", function(req, res){
 
 // POST /todos
 app.post("/todos", function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, "description", "completed"); // use _.pick to only pick description and completed
 
+
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) 
+		|| body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	//set body.description to be trimmed value.
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
-	//add id field
-	//push body into array
+	
 
 	todos.push(body);
 
